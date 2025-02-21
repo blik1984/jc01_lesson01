d2cf77f59af52e8c77854f691293a4b84ae74404
Aleksandr Klimov <70091133+blik1984@users.noreply.github.com>
2025-02-21 16:12:02 +0300

    Add files via upload

---
+++ jc01_lesson01/src/edu/training/jc/simple_output/FrameOutput.java
@@ -0,0 +1,10 @@
+package edu.training.jc.simple_output;
+
+public class FrameOutput {
+
+	public static void main(String[] args) {
+		System.out.println("*****************\n*\t*\t*\n*\t*\t*\n*\t*\t*\n*****************");
+
+	}
+
+}
---
+++ jc01_lesson01/src/edu/training/jc/simple_output/SimpleConsoleOutputOne.java
@@ -0,0 +1,13 @@
+
+
+package edu.training.jc.simple_output;
+
+public class SimpleConsoleOutputOne {
+
+	public static void main(String[] args) {
+		
+		System.out.println("A\nB\nC\nD\nE");
+
+	}
+
+}
